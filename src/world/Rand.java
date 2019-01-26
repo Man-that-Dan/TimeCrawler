189daff43c786cf5deb344c940583e44d911aa4a
homosapien97 <christian.antoni.gutowski@gmail.com>
2019-01-26 16:38:50 -0500

    Added room random number generator

--- src/world/Rand.java
+++ src/world/Rand.java
@@ -5,8 +5,12 @@ import java.util.Random;
 public class Rand {
     public static long seed;
     public static Random world_rand = new Random(seed);
+    public static Random room_rand = new Random(world_rand.nextInt());
 
     public static int world_next_int(int lowbound_inclusive, int highbound_exclusive) {
         return lowbound_inclusive + (int) (world_rand.nextDouble() * highbound_exclusive);
+    }
+    public static double room_next_double() {
+        return room_rand.nextDouble();
     }
 }
