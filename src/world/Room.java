d6bd6dce02ab43c4f62433a3d4585105d09bcafd
Daniel <dtoro@g.clemson.edu>
2019-01-26 19:10:08 -0500

    room generation

--- src/world/Room.java
+++ src/world/Room.java
@@ -17,28 +17,28 @@ public class Room {
 
 
     public Room(int width, int height) {
-//TODO
-    //use x and y as height and width to
-    for(int i = 0; i < (Rand.room_rand() % 10); i++){
-        if(longitude > 39.0000){
-            terrain.add(new Terrain((Rand.room_rand % w),(Rand.room_rand % h), "snow_mound"));
-        } else {
-            terrain.add(new Terrain((Rand.room_rand % w),(Rand.room_rand % h)));
-        };
-    }
 
-    boolean setConnection(Room newNeighbor, int pos) {
-        //If the connection doesn't already exists for this room in the specified direction, and if
-        //the connection doesn't already exist for the specified room in the opposite direction
-        if(connections[pos] == null && newNeighbor.connections[(pos + 2) % 4] == null) {
-            //Make the rooms neighbors
-            connections[pos] = newNeighbor;
-            newNeighbor.connections[(pos + 2) % 4] = this;
-            //return success
-            return true;
-        } else {
-            //return failure
-            return false;
+        //use x and y as height and width to
+        for(int i = 0; i < (Rand.room_rand() % 10); i++){
+            if(longitude > 39.0000){
+                terrain.add(new Terrain((Rand.room_rand % w),(Rand.room_rand % h), "snow_mound"));
+            } else {
+                terrain.add(new Terrain((Rand.room_rand % w),(Rand.room_rand % h)));
+            };
         }
-    }
+
+        boolean setConnection(Room newNeighbor, int pos) {
+            //If the connection doesn't already exists for this room in the specified direction, and if
+            //the connection doesn't already exist for the specified room in the opposite direction
+            if(connections[pos] == null && newNeighbor.connections[(pos + 2) % 4] == null) {
+                //Make the rooms neighbors
+                connections[pos] = newNeighbor;
+                newNeighbor.connections[(pos + 2) % 4] = this;
+                //return success
+                return true;
+            } else {
+                //return failure
+                return false;
+            }
+        }
 }
--- src/world/Terrain.java
+++ src/world/Terrain.java
@@ -5,12 +5,14 @@ import entity.Entity;
 public class Terrain extends Entity {
     //type property to decide what to render;
     String type;
+    Rectangle poly;
 
     public Terrain(double x, double y){
       posx = x;
       posy = y;
 
       type = "Rock";
+      poly = Rectangle(x, y, )
     };
 
     public Terrain(double x, double y, String settype){
