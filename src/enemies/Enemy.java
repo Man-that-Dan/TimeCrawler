236951b56c2a9724b28719e5ede75bf1667e0142
Daniel <dtoro@g.clemson.edu>
2019-01-26 21:20:54 -0500

    created mob class to encompass enemies and player

--- src/enemies/Enemy.java
+++ src/enemies/Enemy.java
@@ -1,18 +1,19 @@
 package enemies;
 
+import event.*;
 
-import entity.Entity;
+import entity.*;
 import geometry.Rectangle;
 
 //enemy base class - abstract class
-public class Enemy extends Entity {
+public class Enemy extends mob {
     double difficulty;
     int damage = 5;
     int speed = 5;
 
     public void attack(){
         Rectangle AoE = new Rectangle(this.posx, this.posy, (this.poly.x + 5), (this.poly.y + 5)  );
-        //TODO create event with this AoE to see if it intersects the player
+        Event attack = new AttackEvent(this, AoE, damage * difficulty);
     };
 
 }
